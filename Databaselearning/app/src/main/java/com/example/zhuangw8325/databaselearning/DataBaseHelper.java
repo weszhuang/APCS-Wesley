5741526666b73b4d4e42bb50f7086b9a7e82e3fc
weszhuang <zaganon@gmail.com>
2017-05-16 11:14:19 -0700

    5/16/17 Database size increase and viewing implementation

--- Databaselearning/app/src/main/java/com/example/zhuangw8325/databaselearning/DataBaseHelper.java
+++ Databaselearning/app/src/main/java/com/example/zhuangw8325/databaselearning/DataBaseHelper.java
@@ -1,10 +1,14 @@
 package com.example.zhuangw8325.databaselearning;
 
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import android.content.Context;
 import android.support.v7.app.ActionBar;
+import android.util.Log;
+import android.view.View;
 
 /**
  * Created by zhuangw8325 on 5/12/2017.
@@ -16,6 +20,8 @@ public class DataBaseHelper extends SQLiteOpenHelper{
     public static final String TABLE_NAME = "contact_table";
     public static final String COL_1 = "ID";
     public static final String COL_2 = "Name";
+    public static final String COL_3 = "Age";
+    public static final String COL_4 = "Address";
 
     public DataBaseHelper(Context context){
         super(context, DATABASE_NAME, null, 1);
@@ -24,13 +30,29 @@ public class DataBaseHelper extends SQLiteOpenHelper{
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID_INTEGER_PRIMARY_KEY_AUTOINCREMENT, NAME TEXT)");
-
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
+    }
+
+    public boolean insertData(String name, String age, String address){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2,name);
+        contentValues.put(COL_3,age);
+        contentValues.put(COL_4,address);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        return !(result == -1);
+    }
+
+    public Cursor getAllData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from" + TABLE_NAME, null);
+        return res;
     }
 }
 
--- Databaselearning/app/src/main/java/com/example/zhuangw8325/databaselearning/MainActivity.java
+++ Databaselearning/app/src/main/java/com/example/zhuangw8325/databaselearning/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.zhuangw8325.databaselearning;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -12,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
 
     DataBaseHelper myDb;
     EditText editName;
+    EditText editAge;
+    EditText editAddress;
     Button btnAddData;
 
 
@@ -21,5 +26,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myDb = new DataBaseHelper(this);
+    }
+
+
+
+    public void addData (View v){
+        Log.d("MyContact", "Adding Data");
+        boolean isInserted = myDb.insertData(editName.getText().toString(),editAge.getText().toString(),editAddress.getText().toString());
+
+        if(isInserted) {
+            Log.d("MyContact", "Success Inserting data");
+        }else{
+            Log.d("MyContact", "Failure Inserting data");
+        }
+    }
+
+    public void viewData(View v){
+        Cursor res = myDb.getAllData();
+        if(res.getCount() == 0){
+            showMessage("Error", "No data found in database");
+
+        }
+
+
+        StringBuffer buffer = new StringBuffer();
+
+        showMessage("Data", buffer.toString());
+    }
+
+    public void showMessage(String s, String b){
+
     }
 }
