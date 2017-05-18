ac0f0eda634c76140b457ad46dfe54e075ac197a
weszhuang <zaganon@gmail.com>
2017-05-18 10:29:18 -0700

    Searching functions and database update

--- Databaselearning/app/src/main/java/com/example/zhuangw8325/databaselearning/DataBaseHelper.java
+++ Databaselearning/app/src/main/java/com/example/zhuangw8325/databaselearning/DataBaseHelper.java
@@ -14,44 +14,47 @@ import android.view.View;
  * Created by zhuangw8325 on 5/12/2017.
  */
 
-public class DataBaseHelper extends SQLiteOpenHelper{
 
+public class DataBaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "Contact.db";
     public static final String TABLE_NAME = "contact_table";
-    public static final String COL_1 = "ID";
-    public static final String COL_2 = "Name";
-    public static final String COL_3 = "Age";
-    public static final String COL_4 = "Address";
-
-    public DataBaseHelper(Context context){
+    public static final String col_1 = "ID";
+    public static final String col_2 = "NAME";
+    public static final String col_3 = "AGE";
+    public static final String col_4 = "ADDRESS";
+    public DataBaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID_INTEGER_PRIMARY_KEY_AUTOINCREMENT, NAME TEXT)");
+
+        db.execSQL("CREATE TABLE " + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, AGE TEXT, ADDRESS TEXT)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+
+        db.execSQL("DROP TABLE IF EXISTS" + TABLE_NAME);
         onCreate(db);
     }
-
-    public boolean insertData(String name, String age, String address){
+    public boolean insertData(String name, String age, String address) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(COL_2,name);
-        contentValues.put(COL_3,age);
-        contentValues.put(COL_4,address);
-
-        long result = db.insert(TABLE_NAME, null, contentValues);
-        return !(result == -1);
+        contentValues.put(col_2,name);
+        contentValues.put(col_3,age);
+        contentValues.put(col_4,address);
+        long result = db.insert(TABLE_NAME,null,contentValues);
+        if (result == -1 ) {
+            return false;
+        }
+        else {
+            return true;
+        }
     }
-
-    public Cursor getAllData(){
+    public Cursor getAllData() {
         SQLiteDatabase db = this.getWritableDatabase();
-        Cursor res = db.rawQuery("select * from" + TABLE_NAME, null);
+        Cursor res = db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
         return res;
     }
 }
--- Databaselearning/app/src/main/java/com/example/zhuangw8325/databaselearning/MainActivity.java
+++ Databaselearning/app/src/main/java/com/example/zhuangw8325/databaselearning/MainActivity.java
@@ -1,26 +1,25 @@
 package com.example.zhuangw8325.databaselearning;
 
-import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.database.Cursor;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
 
-/**
- * Created by zhuangw8325 on 5/12/2017.
- */
 public class MainActivity extends AppCompatActivity {
 
     DataBaseHelper myDb;
     EditText editName;
     EditText editAge;
     EditText editAddress;
+    EditText editSearch;
     Button btnAddData;
-
+    String[] fields;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,48 +27,89 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myDb = new DataBaseHelper(this);
+        //add the layout bars
+        editName = (EditText) findViewById(R.id.editText_Name);
+        editAge = (EditText) findViewById(R.id.editText_Age);
+        editAddress = (EditText) findViewById(R.id.editText_address);
+        editSearch = (EditText) findViewById(R.id.editSearch);
+        fields = new String[4];
+        fields[0] = "ID: ";
+        fields[1] = "Name: ";
+        fields[2] = "Age: ";
+        fields[3] = "Address: ";
     }
+    public void addData(View v) {
+        boolean isInserted = (myDb.insertData(editName.getText().toString(),editAge.getText().toString(),editAddress.getText().toString()));
 
-
-
-    public void addData (View v){
-        Log.d("MyContact", "Adding Data");
-        boolean isInserted = myDb.insertData(editName.getText().toString(),editAge.getText().toString(),editAddress.getText().toString());
-
-        if(isInserted) {
-            Log.d("MyContact", "Success Inserting data");
-        }else{
-            Log.d("MyContact", "Failure Inserting data");
+        String text = "";
+        if (isInserted == true) {
+            Log.d("MyContact", "Success inserting data");
+            text = "Data inserted";
+            Toast toast = Toast.makeText(getApplicationContext(), text,Toast.LENGTH_SHORT);
+            toast.show();
         }
-    }
+        else {
+            text = "Failure";
+            Toast toast = Toast.makeText(getApplicationContext(), text,Toast.LENGTH_SHORT);
+            toast.show();
+            Log.d("MyContact", "Failure inserting data");
+        }
+        editName.setText("");
+        editAge.setText("");
+        editAddress.setText("");
 
-    public void viewData(View v){
+    }
+    public void viewData(View v) {
         Cursor res = myDb.getAllData();
-        if(res.getCount() == 0) {
+        if (res.getCount() == 0) {
             showMessage("Error", "No data found in database");
-            return;
         }
-
         StringBuffer buffer = new StringBuffer();
         res.moveToFirst();
-        for(res.moveToFirst();!res.isAfterLast();res.moveToNext()){
-            for(int i = 0; i<res.getColumnCount();i++){
-                buffer.append(res.getString(i) + "  ");
+        for (int i = 0; i < res.getCount(); i++) {
+            for (int j = 0; j <= 3; j++) {
+                buffer.append(fields[j]);
+                buffer.append(res.getString(j));
+                buffer.append("\n");
             }
-            buffer.append("\n");
+            res.moveToNext();
         }
         showMessage("Data", buffer.toString());
+        System.out.println(buffer);
     }
 
-    public void showMessage(String Title, String message){
+    public void showMessage(String title, String message){
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(message).setTitle(Title)
-            .setPositiveButton("Close", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int id) {
-                //do nothing kek
-            }
-        });
+        builder.setMessage(message).setTitle(title)
+                .setPositiveButton("Close", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        //do nothing kek
+                    }
+                });
         builder.show();
+    }
+
+    public void searchMessage(View v) {
+        String search = editSearch.getText().toString();
+        StringBuffer buffer = new StringBuffer();
+        Cursor res = myDb.getAllData();
+        res.moveToFirst();
+        for (int i = 0; i < res.getCount(); i++) {
+            if (res.getString(1).equals(search)) {
+                for (int j = 0; j < 4; j++) {
+                    buffer.append(fields[j]);
+                    buffer.append(res.getString(j));
+                    buffer.append("\n");
+                }
+            }
+            res.moveToNext();
+        }
+        if (buffer.toString().equals("")) {
+            showMessage("No results found", "");
+        } else {
+            showMessage("Results", buffer.toString());
+        }
+        editSearch.setText("");
     }
 }
